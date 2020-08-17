622f44e4290d16282018e619b58611fc2846caca
Ramil <yourfinalfantasy@yandex.ru>
2020-08-17 17:59:49 +0300

    start commit

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  IMusic
+//
+//  Created by Ramil Davletshin on 13.08.2020.
+//  Copyright © 2020 Ramil Davletshin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+        Text("Hello, World 2!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
