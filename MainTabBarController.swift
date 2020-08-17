c0eae344ca21d5250b6c92c11ea780abf8e5419d
Ramil <yourfinalfantasy@yandex.ru>
2020-08-17 18:58:46 +0300

    MainTabBarController
    added and configured MainTabBarController

---
+++ MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarController.swift
+//  IMusic
+//
+//  Created by Ramil Davletshin on 17.08.2020.
+//  Copyright © 2020 Ramil Davletshin. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        
+        let searchVC = SearchViewController()
+        let navigationSearchVC = generateViewController(rootViewController: searchVC, image: #imageLiteral(resourceName: "search"), title: "Search")
+        let contentView = UIHostingController(rootView: ContentView())
+        let libraryVC = generateViewController(rootViewController: contentView, image: #imageLiteral(resourceName: "library"), title: "Library")
+        viewControllers = [navigationSearchVC, libraryVC]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navigationVC
+    }
+    
+}
---
+++ SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  IMusic
+//
+//  Created by Ramil Davletshin on 17.08.2020.
+//  Copyright © 2020 Ramil Davletshin. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
